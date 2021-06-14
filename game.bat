8d21e82edca029c72af85e8638c1552d6e21c34b
Willy <willybc06@gmail.com>
2021-06-14 03:50:48 -0300

    Game bat

---
+++ game.bat
@@ -0,0 +1,18 @@
+tasm limpiar.asm
+tlink /t limpiar
+limpiar
+
+tasm game.asm
+tasm salto.asm
+tasm menu.asm
+tasm contador.asm
+tasm copioV.asm
+tasm hitStand.asm
+
+tasm carta.asm
+tasm generaC.asm
+tasm regasci2.asm
+tasm asciireg.asm
+
+tlink game salto menu contador copioV carta hitStand generaC regasci2 asciireg
+game
